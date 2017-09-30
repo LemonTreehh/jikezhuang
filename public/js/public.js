701f8a7d6e0e6c28bb348dc5802e4c39d5e1326f
kkoala <1391680048@qq.com>
2017-09-30 21:48:52 +0800

    public.js文件

---
+++ public/js/public.js
@@ -0,0 +1,13 @@
+
+
+!(function(){
+
+    var fontSize = window.innerWidth / (750 / 100);
+    // console.log( window.innerWidth, document.documentElement.clientWidth )
+
+    var html = document.querySelector('html')
+    html.style.fontSize = fontSize + 'px'
+    
+    
+
+})();
